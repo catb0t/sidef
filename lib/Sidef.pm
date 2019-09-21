7d814f1439f71daddf8b66b8fe1a28ad10654454
Cat Stevens <catb0t@protonmail.ch>
2019-09-21 14:18:52 -0400

    temp

--- lib/Sidef.pm
+++ lib/Sidef.pm
@@ -456,8 +456,9 @@ package Sidef {
 
     my $table   = \%{$self . '::'};
     my @methods = grep { ( !ref($table->{$_}) ) and defined(&{$table->{$_}}) } keys(%$table);
-    my $method  = Sidef::normalize_method($AUTOLOAD);
-    my $name    = substr($method, rindex($method, q{.}) + 1);
+
+    my $method = Sidef::normalize_method($AUTOLOAD);
+    my $name   = substr($method, rindex($method, q{.}) + 1);
 
     my @candidates = Sidef::best_matches($name, \@methods);
 
